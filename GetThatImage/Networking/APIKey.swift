14aefa9b5d2997350a53e43b030313dada98835c
Prakhar Tripathi <prakhar.chanu30@gmail.com>
2021-03-06 19:50:19 +0530

    added template api key

---
+++ GetThatImage/Networking/APIKey.swift
@@ -0,0 +1,12 @@
+//
+//  APIKey.swift
+//  GetThatImage
+//
+//  Created by Prakhar Tripathi on 06/03/21.
+//
+
+import Foundation
+
+struct APIKey {
+    static let key = "INSERT YOUR API KEY HERE"
+}
